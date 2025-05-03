1f2e45533be653265e91cba02ce98e3fc4fc75e8
Kelson-D-Pedro <kelsonpedro016@gmail.com>
2025-05-03 17:41:37 +0100

    testing the classes in ex01

--- cpp00/ex01/main.cpp
+++ cpp00/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: kpedro <kpedro@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/03 16:19:52 by kpedro            #+#    #+#             */
-/*   Updated: 2025/05/03 16:47:48 by kpedro           ###   ########.fr       */
+/*   Updated: 2025/05/03 17:40:17 by kpedro           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,17 @@ int main()
     PhoneBook phonebook;
     Contact contact;
     std::string comand;
+    std::string teste;
 
     do
     {
-        /* code */
-    } while ((comand.compare("EXIT") != 0) || (comand.compare("exit") != 0));
-    
+        std::cin >> comand;
+        if (comand.compare("ADD") == 0 || comand.compare("add") == 0)
+        {
+            std::cin >> teste;
+            contact.set_fname(teste);
+        }
+    } while ((comand.compare("EXIT") != 0) && (comand.compare("exit") != 0));
     
 
     return 0;
